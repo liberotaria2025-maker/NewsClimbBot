3d8287984ea29042777421e5236b67b7da45a153
liberotaria2025-maker <liberotaria2025@gmail.com>
2025-08-30 12:37:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import time
+
 if _name_ == "_main_":
-    login(driver)
-    buscar_y_retweet(driver)
-    driver.quit()
+    while True:
+        try:
+            login(driver)
+            buscar_y_retweet(driver)
+            driver.quit()
+        except Exception as e:
+            print("Error:", e)
+
+        # esperar 1 hora
+        time.sleep(3600)
