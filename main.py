e0a32da423a29996a2f3ca40a7b1098c447aa34a
liberotaria2025-maker <liberotaria2025@gmail.com>
2025-08-30 12:22:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ client = tweepy.Client(
 )
 
 # === Palabra clave a buscar ===
-KEYWORD = "tu_palabra"  # <-- cambia por la palabra que quieras
+KEYWORD = "vllc"  # <-- cambia por la palabra que quieras
 
 def buscar_y_retweet():
     # Buscar tweets recientes con la palabra
@@ -31,5 +31,5 @@ def buscar_y_retweet():
             except Exception as e:
                 print(f"Error con tweet {t.id}: {e}")
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     buscar_y_retweet()
